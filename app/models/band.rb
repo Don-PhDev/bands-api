82c17bb2fa396772926c5d11eb25a520867cc8d7
Raymond <raymond@philippinedev.com>
2021-10-25 14:23:29 +0800

    added model band tests

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,4 +1,6 @@
 class Band < ApplicationRecord
   belongs_to :user
   has_many :members, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true
 end
--- spec/models/band_spec.rb
+++ spec/models/band_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Band, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+  end
+
   describe 'relationship' do
     it { should belong_to(:user) }
     it { should have_many(:members) }

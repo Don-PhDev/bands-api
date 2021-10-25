001d4ac8def48ebccb240f841e07d4798d143286
Raymond <raymond@philippinedev.com>
2021-10-25 15:06:35 +0800

    added email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,9 @@ class User < ApplicationRecord
   has_many :bands, dependent: :destroy
   has_many :members, dependent: :destroy
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true,
+    format: {
+      with: /\A[^@\s\W]+@[^@\s\W]+\z/,
+      message: "must be a valid email"
+    }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe User, type: :model do
 
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
+    it { should allow_value("hello@world").for(:email) }
   end
 
   describe 'relationship' do
